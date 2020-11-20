ad42a35603e2c559ed996110acdd448da2871733
Enrique Coscarelli <enriquecoscarelli@gmail.com>
2020-11-20 11:52:06 +0100

    Skeleton of project

--- models/User.model.js
+++ models/User.model.js
@@ -1,1 +1,19 @@
 // Model
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: { type: String, unique: true },
+  password: String
+},
+{
+  timestamps: {
+    createdAt: 'create_at',
+    updatedAt: 'updated_at'
+  }
+}
+)
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
