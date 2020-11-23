061e9c15ca275cc86102905500665f3afc172ca2
Mirko Siddi <mirko.siddi@gmail.com>
2020-11-23 08:46:49 +0100

    Added 2 selects more to the filter, now there are class type, trainer, duration, difficulty

--- views/components/Filter.js
+++ views/components/Filter.js
@@ -9,23 +9,53 @@ function Filter(props) {
   let classesArrTypes = classesArr.map((elem) => {
     return elem.classType;
   });
+  let classDurationArr = classesArr.map((elem) => {
+    return elem.duration;
+  });
+  let classDifficultyArr = classesArr.map((elem) => {
+    return elem.difficulty;
+  });
   const uniqueTrainers = unifyArray(trainersArr);
   const uniqueClassesTypes = unifyArray(classesArrTypes);
-  console.log("uniqueClasses", uniqueClassesTypes);
+  const uniqueClassDuration = unifyArray(classDurationArr);
+  const uniqueDifficulty = unifyArray(classDifficultyArr);
+  console.log("uniqueDifficulty", uniqueDifficulty);
   return (
     <div id="filter" className="filter">
       <h3>Filter</h3>
       <form>
-        <select type="select" name="classType">
+        <label for="classType">Class type</label>
+        <select name="classType" id="classType">
+          <option value="">Select class type</option>
           {uniqueClassesTypes.map((elem) => {
             return <option value={elem}>{elem}</option>;
           })}
         </select>
         <br></br>
         <br></br>
-        <select name="trainer">
+        <label for="trainer">Trainer</label>
+        <select name="trainer" id="trainer">
+          <option value="">Select Trainer</option>
           {uniqueTrainers.map((elem) => {
             return <option value={elem._id}>{elem.name}</option>;
+          })}
+        </select>{" "}
+        <br></br>
+        <br></br>
+        <label for="duration">Duration</label>
+        <select name="duration">
+          <option value="">Select duration</option>
+          {uniqueClassDuration.map((elem) => {
+            return <option value={elem}>{elem}</option>;
+          })}
+        </select>{" "}
+        <br></br>
+        <br></br>
+        <label for="difficulty">Difficulty</label>
+        <select name="difficulty">
+          <option value="">Select difficulty</option>
+          {uniqueDifficulty.map((elem) => {
+            return <option value={elem}>{elem}</option>;
           })}
         </select>
       </form>
