31522736e12c9144dd11f0cc3d1fdb23831b0351
Mirko Siddi <mirko.siddi@gmail.com>
2020-11-24 07:47:34 +0100

    moved the isBooked logic to the siteRouter with nested promises. To check with Uros

--- views/components/GenerateScriptDivFilter.js
+++ views/components/GenerateScriptDivFilter.js
@@ -1,22 +1,38 @@
 const React = require("react");
+
 const Class = require("../../modules/Class");
 
 function GenerateScriptDivFilter(props) {
-  const classesArr = props;
-  classesArr.map((obj, i) => {
-      const variableName="classObj"+i;
-      const variableName = new Class(  obj.trainer,
-        obj.name,
-        obj.description,
-        obj.closureMessage,
-        obj.scheduled,
-        obj.duration,
-        obj.classType,
-        obj.difficulty,
-        obj.url,
-        obj.equipment);
-  }
+  const classesArr = props.classesArr.foundClasses;
+  //   console.log("classesArr", classesArr);
+  let divsArr = [];
+  let actualDay = null;
 
+  classesArr.map((obj, i) => {
+    let divObjToInsert = new Object();
+    obj.scheduled != actualDay ? (actualDay = obj.scheduled) : null;
+    const equipmentArr = obj.equipment.map((equipObj) => {
+      return { equipObj };
+    });
+    const id =
+      "class-" + i + "-" + actualDay.getDate() + "" + actualDay.getMonth();
+    const className =
+      "class " + actualDay.getDate() + "" + actualDay.getMonth();
+
+    divObjToInsert.id = id;
+    divObjToInsert.className = className;
+    divObjToInsert.name = obj.name;
+    divObjToInsert.scheduled = obj.scheduled;
+    divObjToInsert.trainerName = obj.trainer.name;
+    divObjToInsert.trainerId = obj.trainer._id;
+    divObjToInsert.duration = obj.duration;
+    divObjToInsert.classType = obj.classType;
+    divObjToInsert.equipment = equipmentArr;
+    divsArr.push(divObjToInsert);
+  });
+  const stringDivs = JSON.stringify(divsArr);
+  console.log("stringDivs", stringDivs);
+  //   return <script>const stringDivs={{ stringDivs }}</script>;
   return <div></div>;
 }
 
