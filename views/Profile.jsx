5ca23d9508cb7ac1f2a503d78524365c2b4ef475
Mirko Siddi <mirko.siddi@gmail.com>
2020-11-25 11:42:40 +0100

    Changed Submit button to anchor tag for logout inside profile

--- views/Profile.jsx
+++ views/Profile.jsx
@@ -1,47 +1,43 @@
 const React = require("react");
 const Layout = require("./Layout");
 
-function Profile (props) {
-    console.log(props)
-    return(
-        <Layout title="Profile">
-            <form id="form" action="/private/profile" method="POST">
-                <i href="{props.user[0].picUrl}">PROFILE PICTURE</i>
-                
-                <button type="submit">LOG OUT</button>
-                <button type="submit">EDIT</button>
-                <br/>
-                <h2>NAME</h2>
-                <p>{props.user[0].name}</p>
-                <br/>
-                <h2>LAST NAME</h2>
-                <p>{props.user[0].lasName}</p>
-                <br/>
-                <h2>EMAIL</h2>
-                <p>{props.user[0].email}</p>
-                <br/>
-                <h2>GENDER</h2>
-                <p>{props.user[0].gender}</p>
-                <br/>
-                <h2>BIRTHDATE</h2>
-                <p>{props.user[0].birthDate}</p>
-                <br/>
-                <h2>CITY</h2>
-                <p>{props.user[0].city}</p>
-                <br/>
-                <h2>COUNTRY</h2>
-                <p>{props.user[0].country}</p>
-                <br/>
+function Profile(props) {
+  console.log(props);
+  return (
+    <Layout title="Profile">
+      <form id="form" action="/private/profile" method="POST">
+        <i href="{props.user[0].picUrl}">PROFILE PICTURE</i>
+        <a href="/auth/logout">Logout</a>
+        <button type="submit">EDIT</button>
+        <br />
+        <h2>NAME</h2>
+        <p>{props.user[0].name}</p>
+        <br />
+        <h2>LAST NAME</h2>
+        <p>{props.user[0].lasName}</p>
+        <br />
+        <h2>EMAIL</h2>
+        <p>{props.user[0].email}</p>
+        <br />
+        <h2>GENDER</h2>
+        <p>{props.user[0].gender}</p>
+        <br />
+        <h2>BIRTHDATE</h2>
+        <p>{props.user[0].birthDate}</p>
+        <br />
+        <h2>CITY</h2>
+        <p>{props.user[0].city}</p>
+        <br />
+        <h2>COUNTRY</h2>
+        <p>{props.user[0].country}</p>
+        <br />
 
-                {
-                props.errorMessage 
-                    ? <div className="error-message"> {props.errorMessage} </div>
-                    : null
-                }
-            </form>
-        </Layout>
-    )
+        {props.errorMessage ? (
+          <div className="error-message"> {props.errorMessage} </div>
+        ) : null}
+      </form>
+    </Layout>
+  );
 }
 
 module.exports = Profile;
-
