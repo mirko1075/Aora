ef68befe7b7d0f80ed1c6cf236a9ce7492873b25
Mirko Siddi <mirko.siddi@gmail.com>
2020-11-26 17:35:30 +0100

    Home page create

--- views/Home.jsx
+++ views/Home.jsx
@@ -4,22 +4,9 @@ const Layout = require("./Layout");
 function Home() {
   return (
     <Layout title="Home Page">
-      <svg
-        id='Layer_1” data-name=“Layer 1" xmlns=“http://www.w3.org/2000/svg'
-        viewBox="0 0 405 455"
-      >
-        <polygon points="0 362 0 0 362 0 0 362" />
-        <polygon points="405 93 405 455 43 455 405 93" />
-      </svg>
-      <svg
-        id="Layer_2"
-        data-name="Layer 2"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 405 455"
-      >
-        <polygon points="0 362 0 0 362 0 0 362" />
-        <polygon points="405 93 405 455 43 455 405 93" />
-      </svg>
+      <div id="home">
+        <img src="/images/logo.svg" className="logo" />
+      </div>
     </Layout>
   );
 }
--- views/Profile.jsx
+++ views/Profile.jsx
@@ -1,39 +1,39 @@
 const React = require("react");
 const Layout = require("./Layout");
 
-function Profile (props) {
-    return(
-        <Layout title="Profile">
-            <form id="form" action="/private/profile" method="POST">
-                <img href="{props.user[0].picUrl}">PROFILE PICTURE</img>
-                <a href="/auth/logout">Log out</a>
-                <a href="/private/profileform">Edit profile</a>
-                <br/>
-                <h2>NAME</h2>
-                <p>{props.user.name}</p>
-                <br/>
-                <h2>LAST NAME</h2>
-                <p>{props.user.lastName}</p>
-                <br/>
-                <h2>EMAIL</h2>
-                <p>{props.user.email}</p>
-                <br/>
-                <h2>GENDER</h2>
-                <p>{props.user.gender}</p>
-                <br/>
-                <h2>BIRTHDATE</h2>
-                {/* <p>{props.user.birthDate}</p> */}
-                {props.user.birthDate ? <p>{props.user.birthDate.toString()}</p> : null}
-                <br/>
-                <h2>CITY</h2>
-                <p>{props.user.city}</p>
-                <br/>
-                <h2>COUNTRY</h2>
-                <p>{props.user.country}</p>
-                <br/>
-                {props.errorMessage ? (
-                <div className="error-message"> {props.errorMessage} </div>
-                ) : null}
+function Profile(props) {
+  return (
+    <Layout title="Profile">
+      <form id="form" action="/private/profile" method="POST">
+        <img src={props.user.picUrl}>PROFILE PICTURE</img>
+        <a href="/auth/logout">Log out</a>
+        <a href="/private/profileform">Edit profile</a>
+        <br />
+        <h2>NAME</h2>
+        <p>{props.user.name}</p>
+        <br />
+        <h2>LAST NAME</h2>
+        <p>{props.user.lastName}</p>
+        <br />
+        <h2>EMAIL</h2>
+        <p>{props.user.email}</p>
+        <br />
+        <h2>GENDER</h2>
+        <p>{props.user.gender}</p>
+        <br />
+        <h2>BIRTHDATE</h2>
+        {/* <p>{props.user.birthDate}</p> */}
+        {props.user.birthDate ? <p>{props.user.birthDate.toString()}</p> : null}
+        <br />
+        <h2>CITY</h2>
+        <p>{props.user.city}</p>
+        <br />
+        <h2>COUNTRY</h2>
+        <p>{props.user.country}</p>
+        <br />
+        {props.errorMessage ? (
+          <div className="error-message"> {props.errorMessage} </div>
+        ) : null}
       </form>
     </Layout>
   );
